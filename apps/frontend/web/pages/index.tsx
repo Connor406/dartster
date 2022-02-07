63ff25f2cb56b8504de1f034cc51b6cc83f95df5
Connor Mahan <mahan406@gmail.com>
2022-02-06 23:25:18 -0600

    mobile bg fix

--- apps/frontend/web/pages/index.tsx
+++ apps/frontend/web/pages/index.tsx
@@ -7,7 +7,7 @@ const MotionText = motion(Text)
 
 export function Index() {
   return (
-    <Box h="100vh" w="100vw" textAlign="center">
+    <Box minH="100vh" minW="100vw" textAlign="center">
       <LandingTriangle />
       <Flex h="50vh" w="100vw" justifyContent="center" alignItems="flex-end" textAlign="center">
         <Logo />
